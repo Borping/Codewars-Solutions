1703e2d777194799d912683f40005ded2e06b080
Borping <163424538+Borping@users.noreply.github.com>
2025-10-30 04:17:01 -0500

    Create StringEndsWith.py

---
+++ StringEndsWith.py
@@ -0,0 +1,18 @@
+"""Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+Inputs: "abc", "bc"
+Output: true
+
+Inputs: "abc", "d"
+Output: false"""
+
+def solution(text, ending):
+    if len(ending) > len(text):
+        return False
+    for i in range(len(ending)):
+        spot = ((i+1) * -1)
+        if not text[spot] == ending[spot]:
+            return False
+    return True
